e3c9dfa542f378ea99be48ea59ec5bb1663a23e3
echo <edward@eparry.uk>
2025-09-24 23:21:04 +0100

    Got looping working by moving to the instance interface for vlc

--- src/toddler_transducer/audio.py
+++ src/toddler_transducer/audio.py
@@ -6,18 +6,22 @@ Contains all the code to load, play, stop the audio to play
 This uses the py vlc interface, api reference, https://www.olivieraubert.net/vlc/python-ctypes/doc/.
 
 """
-
+import time
 from typing import Optional
 
 import vlc
 from pathlib import Path
 
-from .audio_file_manager import get_current_files
 from .config import AUDIO_FILE_BASE_PATH
 from .metadata import load_metadata
 
 # Starting the vlc instance
-VLC_MEDIA_PLAYER= vlc.MediaPlayer()
+# VLC_MEDIA_PLAYER= vlc.MediaPlayer()
+
+VLC_MEDIA_INSTANCE = vlc.Instance()
+VLC_MEDIA_LIST_PLAYER = VLC_MEDIA_INSTANCE.media_list_player_new()
+
+LOOPING = False
 
 
 def load_track(rfid_tag: Optional[str] = None, track_name: Optional[str] = None):
@@ -28,11 +32,35 @@ def load_track(rfid_tag: Optional[str] = None, track_name: Optional[str] = None)
     else:
         raise TypeError('Must provide either rfid_tag or track_name')
     print(audio_path)
-    VLC_MEDIA_PLAYER.set_mrl(audio_path)
-    VLC_MEDIA_PLAYER.play()
+    global VLC_MEDIA_PLAYER
+    VLC_MEDIA_LIST_PLAYER.stop()
+    media = VLC_MEDIA_INSTANCE.media_new(audio_path)
+    media_list = VLC_MEDIA_INSTANCE.media_list_new()
+    media_list.add_media(media)
+    VLC_MEDIA_LIST_PLAYER.set_media_list(media_list)
+    VLC_MEDIA_LIST_PLAYER.play()
+
+
+def play_vlc():
+    VLC_MEDIA_LIST_PLAYER.play()
+
+
+def pause_vlc():
+    VLC_MEDIA_LIST_PLAYER.pause()
+
+
+def toggle_loop_vlc():
+    global LOOPING
+    VLC_MEDIA_LIST_PLAYER.set_playback_mode(int(not LOOPING))
+    LOOPING = not LOOPING
+
+
+def get_looping():
+    return LOOPING
+
 
 def get_playing_track():
-    media = VLC_MEDIA_PLAYER.get_media()
+    media = VLC_MEDIA_LIST_PLAYER.get_media_player().get_media()
     if media is None:
         return None
     mrl = Path(media.get_mrl())
@@ -42,4 +70,18 @@ def get_playing_track():
 
 
 def is_playing():
-    return VLC_MEDIA_PLAYER.is_playing() == 1
+    return VLC_MEDIA_LIST_PLAYER.get_media_player().is_playing() == 1
+
+
+def seconds_to_mmss(seconds):
+    return time.strftime('%M:%S', time.gmtime(seconds))
+
+
+def get_track_length():
+    track_length = seconds_to_mmss(VLC_MEDIA_LIST_PLAYER.get_media_player().get_length() / 1000)
+    return track_length
+
+
+def get_track_time():
+    play_time = seconds_to_mmss(VLC_MEDIA_LIST_PLAYER.get_media_player().get_time() / 1000)
+    return play_time
--- src/toddler_transducer/web_ui/root.py
+++ src/toddler_transducer/web_ui/root.py
@@ -8,7 +8,8 @@ from werkzeug.utils import secure_filename
 from .app import flask_app
 from .html_templates import PLAY_BUTTON, PAUSE_BUTTON
 from ..audio_file_manager import get_current_files
-from ..audio import load_track, get_playing_track, is_playing
+from ..audio import load_track, get_playing_track, is_playing, get_track_length, get_track_time, pause_vlc, play_vlc, \
+    toggle_loop_vlc, LOOPING, get_looping
 from ..config import AUDIO_FILE_BASE_PATH
 from ..metadata import load_metadata, save_metadata
 from ..rfid import get_rfid_id
@@ -26,21 +27,41 @@ def home() -> str:
         track_name = current_track_metadata['track_name']
 
     if is_playing():
+        play_status = 'Playing'
         play_status_icon = PAUSE_BUTTON
+        track_length = get_track_length()
+        track_time = get_track_time()
     else:
+        play_status = 'Paused'
         play_status_icon = PLAY_BUTTON
+        track_length = "00:00"
+        track_time = "00:00"
+
+    if get_looping():
+        loop_icon_class = 'icon-container-looping'
+    else:
+        loop_icon_class = 'icon-container'
 
     html_files = render_template('index.html',
                                  playable_tracks=track_names,
                                  playable_tracks_list=json.dumps(track_names),
                                  track_name=track_name,
-                                 play_status_icon=play_status_icon)
+                                 play_status=play_status,
+                                 play_status_icon=play_status_icon,
+                                 play_track_length=track_length,
+                                 play_current_time=track_time,
+                                 loop_icon_class=loop_icon_class)
     return html_files
 
 @flask_app.route('/play_track', methods=['POST'])
 def play_track():
+
     playable_tracks = get_current_files()
     if request.form['AudioTrackName'] not in playable_tracks:
+        if 'Playing' in request.form:
+            pause_vlc()
+        else:
+            play_vlc()
         return redirect(request.referrer)
     # Play the audio track
     load_track(track_name=playable_tracks[request.form['AudioTrackName']])
@@ -65,4 +86,9 @@ def upload_track():
         save_metadata(metadata)
         file.save(AUDIO_FILE_BASE_PATH / file_name)
 
+    return redirect(request.referrer)
+
+@flask_app.route('/loop_track', methods=['POST'])
+def loop_track():
+    toggle_loop_vlc()
     return redirect(request.referrer)
