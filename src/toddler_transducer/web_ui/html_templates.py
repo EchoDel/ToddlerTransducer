d5a1205444e088e9545470d9a3824aa4261363a7
echo <edward@eparry.uk>
2025-09-24 23:22:47 +0100

    Added some missing files from the last release

---
+++ src/toddler_transducer/web_ui/html_templates.py
@@ -0,0 +1,3 @@
+
+PLAY_BUTTON = '<polygon class="icon-play" points="19.05 12 6 3.36 6 20.64 19.05 12"/>'
+PAUSE_BUTTON = '<g><rect  class="icon-pause" x="6" y="3.26" width="4" height="17.48"/><rect class="icon-pause" x="14" y="3.26" width="4" height="17.48"/></g>'
