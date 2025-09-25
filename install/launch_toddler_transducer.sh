4367e6d440958d8406ecdc3ba8a00b996d7fea38
echo <edward@eparry.uk>
2025-09-25 20:45:41 +0100

    First pass of the ota

---
+++ install/launch_toddler_transducer.sh
@@ -0,0 +1,1 @@
+poetry run launch_toddler_transducer
---
+++ install/ota_update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Check for updates
+cd ../
+git fetch
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse @{u})
+if [ $LOCAL != $REMOTE ]; then
+echo "Repository is outdated. Updating…"
+# git pull
+# Download updates
+# git checkout master
+git pull
+# Stop the service
+sudo systemctl stop your_application.service
+# Build and update the requirements and package
+poetry install
+poetry build
+# Restart the application
+sudo systemctl start your_application.service
+else
+echo "Repository is up to date."
+fi
