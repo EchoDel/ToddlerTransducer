4b883658b72b6b39dc7e03ee8cd92bbd54666201
echo <edward@eparry.uk>
2025-09-26 16:11:51 +0100

    Tried to use waitress to load the app

--- install/launch_toddler_transducer.sh
+++ install/launch_toddler_transducer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/ToddlerTransducer
 /home/pi/.local/bin/poetry install --with deployment
-/home/pi/.local/bin/poetry run waitress-serve --call 'toddler_transducer:launch_toddler_transducer'
+/home/pi/.local/bin/poetry run launch_toddler_transducer
