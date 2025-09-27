7656f6946800ff5f38d527ad2f056832205488e6
echo <edward@eparry.uk>
2025-09-27 22:05:33 +0100

    Corrected the logic for the main loop

--- src/toddler_transducer/__main__.py
+++ src/toddler_transducer/__main__.py
@@ -1,16 +1,26 @@
+import time
+
 from toddler_transducer.audio import load_track, is_playing, stop_vlc
-from toddler_transducer.metadata import load_metadata
 from toddler_transducer.rfid import get_rfid_id
 
 
 def main():
     current_tag_id = None
+    puck_remove_count = 0
     while True:
         rfid_tag = get_rfid_id()
-        if rfid_tag != current_tag_id:
+        if rfid_tag is None:
+            if puck_remove_count > 3:
+                if is_playing():
+                    stop_vlc()
+                    current_tag_id = rfid_tag
+            else:
+                puck_remove_count += 1
+        elif rfid_tag != current_tag_id:
             if rfid_tag is not None:
                 load_track(rfid_tag=rfid_tag)
                 current_tag_id = rfid_tag
-            else:
-                if is_playing():
-                    stop_vlc()
+                puck_remove_count = 0
+        elif rfid_tag != current_tag_id:
+            puck_remove_count == 0
+        time.sleep(2)
