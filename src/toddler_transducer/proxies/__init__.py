6fc34d03e20b1c9c8afbd3a797163ebbedf1eeaf
Edward Parry <edward@eparry.uk>
2025-10-15 21:45:37 +0100

    Feature/looping fix (#11)
    * Removed the fake rpi package to avoid the random switching of the looping
    
    * Corrected the looping style
    
    * Added the missing package
    
    * Linting issues

--- src/toddler_transducer/proxies/__init__.py
+++ src/toddler_transducer/proxies/__init__.py
@@ -5,3 +5,5 @@ Package containing all the code for proxying things when not on the raspberry pi
 """
 from .multithreading_proxy import MultithreadingValueProxy
 from .mfrc522_proxy import SimpleMFRC522Proxy
+
+__all__ = ['MultithreadingValueProxy', 'SimpleMFRC522Proxy']
