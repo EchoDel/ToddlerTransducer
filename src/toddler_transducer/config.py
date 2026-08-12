e7d9a79e8e7a2ba2e3515989d7902015eaadd658
echo <edward@eparry.uk>
2026-08-12 13:39:12 +0100

    Changed the config to make the paths absolute when initilizing so that other processes know where to find the files. Fixes a crash with the syncing module

--- src/toddler_transducer/config.py
+++ src/toddler_transducer/config.py
@@ -3,16 +3,19 @@ Config
 
 Module containing the config for the application
 """
+
 from pathlib import Path
 
-AUDIO_FILE_BASE_PATH = Path('./audio_files')
-METADATA_FILE_PATH = AUDIO_FILE_BASE_PATH / 'metadata'
-BACKUP_FILE_BASE_PATH = Path('./backups')
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+AUDIO_FILE_BASE_PATH = PROJECT_ROOT / "audio_files"
+METADATA_FILE_PATH = AUDIO_FILE_BASE_PATH / "metadata"
+BACKUP_FILE_BASE_PATH = PROJECT_ROOT / "backups"
 BACKUP_FILE_BASE_PATH.mkdir(parents=True, exist_ok=True)
 
-VOLUME_FILE_PATH = Path('./persistent_settings.json')
+VOLUME_FILE_PATH = PROJECT_ROOT / "persistent_settings.json"
 
-DEVICE_CONFIG_FILE_PATH = Path('./device_config.json')
+DEVICE_CONFIG_FILE_PATH = PROJECT_ROOT / "device_config.json"
 
 WEB_UI_PORT = 8080
 
